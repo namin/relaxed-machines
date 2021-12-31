a6724f74114cd044957ecc3e40f89ff799910e7f
Nada Amin <namin@seas.harvard.edu>
2021-12-31 22:07:06 +0100

    scaffolding

--- dup_add.py
+++ dup_add.py
@@ -1,22 +1,15 @@
-"""A differentiable machine that understands `STOP` and a subset of `INC`, `DEC`, `NOP`, `INC2`.
+"""A differentiable machine that understands `DUP`, `ADD`, `STOP`.
 
-There is no data stack, just a data point.
-There is also a program counter and a code bank.
+The state of the machine comprises a data stack, a program counter and whether the machine has halted.
 
-Internally, the state also keeps track of whether the machine has halted,
-to avoid running instructions after a STOP.
-Without a `halted` state, the machine could get away with diffuse code,
-counting while stopping before the needed incrementing.
+The parameters of the machine are the lines of code.
 
-The code parameterizes the neural network.
-The parameters represent the code, and we learn the code.
-
-The learning task is to learn counting (modulo n) by incrementing d times,
-that is f(x)=(x+d) % n.
-For n=5 and  d=3, we generate the examples
-f(0) = 3, f(1) = 4, f(2) = 0, f(3) = 1, f(4) = 2
+The learning task is to learn multiplication by d modulo n,
+that is f(x)=(x*d) % n.
+For n=5 and  d=2, we generate the examples
+f(0) = 0, f(1) = 2, f(2) = 4, f(3) = 1, f(4) = 3
 and the machine learns the program
-INC, INC, INC, STOP, *
+DUP, ADD, STOP, *
 """
 
 from typing import Any, NamedTuple, Optional
@@ -34,29 +27,15 @@ import optax
 
 import itertools
 
-NOP = flags.DEFINE_boolean('nop', False, 'whether NOP is available as an instruction')
-INC = flags.DEFINE_boolean('inc', True, 'whether INC is available as an instruction')
-DEC = flags.DEFINE_boolean('dec', False, 'whether DEC is available as an instruction')
-INC2 = flags.DEFINE_boolean('inc2', False, 'whether INC2 is available as an instruction')
-INIT_NOP = flags.DEFINE_boolean('init_nop', False, 'whether to intialize with NOPs mostly as opposed to STOPs; only effective with --nop.')
-N = flags.DEFINE_integer('n', 5, 'uniformly, number of integers and number of lines of code')
-D = flags.DEFINE_integer('d', 3, 'learn f(x)=(x+d)%n')
+N = flags.DEFINE_integer('n', 5, 'uniformly, number of integers, number of lines of code and of data stack size')
+D = flags.DEFINE_integer('d', 2, 'learn f(x)=(x*d)%n')
 SOFTMAX_SHARP = flags.DEFINE_float('softmax_sharp', 10, 'the multiplier to sharpen softmax')
 LEARNING_RATE = flags.DEFINE_float('learning_rate', 1e-3, '')
 TRAINING_STEPS = flags.DEFINE_integer('training_steps', 100000, '')
 SEED = flags.DEFINE_integer('seed', 42, '')
 
 def instruction_names():
-    names = ['STOP']
-    if INC.value:
-        names.append('INC')
-    if DEC.value:
-        names.append('DEC')
-    if INC2.value:
-        names.append('INC2')
-    if NOP.value:
-        names.append('NOP')
-    return names
+    return ['DUP', 'ADD', 'STOP']
 
 class Machine(hk.RNNCore):
     def __init__(
@@ -65,80 +44,82 @@ class Machine(hk.RNNCore):
     ):
         super().__init__(name=name)
         self.n = N.value
-        self.has_nop = NOP.value
-        self.has_inc = INC.value
-        self.has_dec = DEC.value
-        self.has_inc2 = INC2.value
-        self.init_nop = self.has_nop and INIT_NOP.value
         self.stop_matrix = jnp.identity(self.n)
         self.inc_matrix =  jnp.roll(jnp.identity(self.n), 1, axis=1)
-        self.data_instructions = [self.stop_matrix]
-        self.pc_instructions = [self.stop_matrix]
-        if self.has_inc:
-            self.data_instructions.append(self.inc_matrix)
-            self.pc_instructions.append(self.inc_matrix)
-        if self.has_dec:
-            self.dec_matrix = jnp.transpose(self.inc_matrix)
-            self.data_instructions.append(self.dec_matrix)
-            self.pc_instructions.append(self.inc_matrix)
-        if self.has_inc2:
-            self.data_instructions.append(jnp.matmul(self.inc_matrix, self.inc_matrix))
-            self.pc_instructions.append(self.inc_matrix)
-        if self.has_nop:
-            self.data_instructions.append(self.stop_matrix)
-            self.pc_instructions.append(self.inc_matrix)
-        self.ni = len(self.data_instructions)
+        self.inames = instruction_names()
+        self.ni = len(self.inames)
 
     def __call__(self, inputs, prev_state):
         new_state = self.step(prev_state)
-        new_data = new_state[0:self.n]
-        new_halted = new_state[2*self.n:2*self.n+2]
-        return (new_data, new_halted), new_state
+        new_data_value = new_state[self.n:2*self.n] # TODO: use the data pointer
+        new_halted = new_state[-3:-1]
+        return (new_data_value, new_halted), new_state
+
+    def is_instr(self, iname, index):
+        return self.inames[index] == iname
+
+    def execute_instr(self, i, data_p, data, pc):
+        if self.is_instr('STOP', i):
+            pass
+        elif self.is_instr('DUP', i):
+            pass
+        # NOP
+        return (data_p, data, jnp.matmul(pc,self.inc_matrix))
 
     def initial_state(self, batch_size: Optional[int]):
-        data = jnp.zeros([self.n]).at[0].set(1)
+        data_p = jnp.zeros([self.n]).at[0].set(1)
+        data = jnp.zeros([self.n, self.n])
+        for i in range(self.n):
+            data = data.at[(i,0)].set(1)
+        data = jnp.reshape(data, self.n*self.n)
         pc = jnp.zeros([self.n]).at[0].set(1)
         halted = jnp.array([0, 1])
-        state = jnp.concatenate((data, pc, halted))
+        state = jnp.concatenate((data_p, data, pc, halted))
         assert batch_size is None
         return state
 
     def step(self, state):
         code = self.get_code()
-        data = jax.nn.softmax(SOFTMAX_SHARP.value*state[0:self.n])
-        pc = jax.nn.softmax(SOFTMAX_SHARP.value*state[self.n:2*self.n])
-        halted = jax.nn.softmax(SOFTMAX_SHARP.value*state[2*self.n:2*self.n+2])
+        data_p = jax.nn.softmax(SOFTMAX_SHARP.value*state[0:self.n])
+        data = state[self.n:self.n*self.n+self.n]
+        data = jnp.reshape(data, [self.n, self.n])
+        for i in range(self.n):
+            data = data.at[i].set(jax.nn.softmax(SOFTMAX_SHARP.value*data[i]))
+        pc = jax.nn.softmax(SOFTMAX_SHARP.value*state[self.n*self.n+self.n:self.n*self.n+2*self.n])
+        halted = jax.nn.softmax(SOFTMAX_SHARP.value*state[self.n*self.n+2*self.n:self.n*self.n+2*self.n+2])
         sel = jnp.zeros(self.ni)
         for i in range(self.n):
             sel += pc[i] * jax.nn.softmax(SOFTMAX_SHARP.value*code[i])
-        data_instr = jnp.reshape(jnp.zeros(self.n*self.n), (self.n, self.n))
-        pc_instr = jnp.reshape(jnp.zeros(self.n*self.n), (self.n, self.n))
+        new_data_p = jnp.zeros(self.n)
+        new_data = jnp.zeros([self.n, self.n])
+        new_pc = jnp.zeros(self.n)
         for i in range(self.ni):
-            data_instr += sel[i] * self.data_instructions[i]
-            pc_instr += sel[i] * self.pc_instructions[i]
-        next_data = halted[0] * data + halted[1] * jnp.matmul(data, data_instr)
-        next_pc = halted[0] * pc + halted[1] * jnp.matmul(pc, pc_instr)
+            (delta_data_p, delta_data, delta_pc) = self.execute_instr(i, data_p, data, pc)
+            new_data_p += sel[i] * delta_data_p
+            new_data += sel[i] * delta_data
+            new_pc += sel[i] * delta_pc
+        next_data_p = halted[0] * data_p + halted[1] * new_data_p
+        next_data = halted[0] * data + halted[1] * new_data
+        next_pc = halted[0] * pc + halted[1] * new_pc
         next_halted = jnp.array([halted[0] + halted[1]*sel[0], halted[1]*sel[1]])
-        next_state = jnp.concatenate((next_data, next_pc, next_halted))
+        next_data = jnp.reshape(next_data, self.n*self.n)
+        next_state = jnp.concatenate((next_data_p, next_data, next_pc, next_halted))
         return next_state
 
     def get_code(self):
         return hk.get_parameter('code', [self.n, self.ni], init=self.make_code_fun())
 
     def make_code_fun(self):
-        if self.init_nop:
-            # all NOPs but last STOP
-            code = jnp.array([[1.0 if (i==0 and (not self.has_nop or l==self.n-1)) or (i==self.ni-1 and (self.has_nop and l!=self.n-1)) else 0.0 for i in range(self.ni)] for l in range(self.n)])
-        else:
-            # all STOPs
-            code = jnp.array([[1.0 if i==0 else 0.0 for i in range(self.ni)] for l in range(self.n)])
+        # all STOPs
+        code = jnp.array([[1.0 if self.is_instr('STOP', i) else 0.0 for i in range(self.ni)] for l in range(self.n)])
 
         def code_fun(shape, dtype):
             return code
         return code_fun
 
-    def load_data(self, state, data):
-        next_state = state.at[0:self.n].set(data)
+    def load_data(self, state, data_value):
+        # assumes the pointer is at the reset
+        next_state = state.at[self.n:self.n*2].set(data_value)
         return next_state
 
 class TrainingState(NamedTuple):
